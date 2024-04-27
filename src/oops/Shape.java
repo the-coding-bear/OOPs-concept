97eb7e417ba69fda6bbcde73a9c6b95e6bdf2d09
the_coding_bear <akanksha060801@gmail.com>
2024-04-27 12:10:32 +0530

    interface is added

---
+++ src/oops/Shape.java
@@ -0,0 +1,20 @@
+package oops;
+
+public interface Shape {
+
+    public static final int i =34;
+
+    public abstract void calculateArea(int r);
+
+}
+
+class Circle implements Shape{
+    public void calculateArea(int r){
+        System.out.println("Area of Circle is " + (Math.PI*r*r));
+    }
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.calculateArea(2);
+    }
+}
