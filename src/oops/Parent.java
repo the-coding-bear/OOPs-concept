2f7c97cf9c43e195c362e2a03556d7175b1297dd
the_coding_bear <akanksha060801@gmail.com>
2024-04-27 11:23:32 +0530

    Modifier is added

--- src/oops/Parent.java
+++ src/oops/Parent.java
@@ -2,10 +2,12 @@ package oops;
 
 public class Parent {
     //overridden method
-    public void m1(){
+    protected void m1(){
         System.out.println("i am m1() of parent");
     }
 }
+
+//public>protected>default>private
 
 class Child extends Parent{
     //overriding method
